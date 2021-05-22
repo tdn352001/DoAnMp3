0862d8fc537a4362996f81cb105e7126488b52ab
Tiến Dũng <83648696+tdn352001@users.noreply.github.com>
2021-05-22 22:53:07 +0700

    giao diện trang người dùng:

---
+++ app/src/main/java/com/example/doanmp3/Fragment/UserFragment.java
@@ -0,0 +1,25 @@
+package com.example.doanmp3.Fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.fragment.app.Fragment;
+import com.example.doanmp3.R;
+
+public class UserFragment extends Fragment {
+    View view;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        view =inflater.inflate(R.layout.fragment_user, container, false);
+        return view;
+    }
+}
